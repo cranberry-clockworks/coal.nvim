6bfb4d9c7859b19644df4919bce9cd1b72970294
Aleksei Abakumkin <cranberry.clockworks@gmail.com>
2025-10-06 09:34:49 +0200

    Colour scheme
    1. Use modern API to set highlight groups.
    2. Lower the contrast for matched parentheses highlighting. This should prevent the terminal cursor being visibly lost.
    3. Use the modern tree sitter groups for the highlighting.
    4. Fix ColorColumn typo

--- lua/coal.lua
+++ lua/coal.lua
@@ -1,263 +1,278 @@
 local M = {}
 
 local function reset()
-    vim.api.nvim_command('hi clear')
-
-    if vim.fn.exists('syntax_on') then
-        vim.cmd('syntax reset')
-    end
-    vim.cmd('set termguicolors')
+	vim.cmd("highlight clear")
+	if vim.fn.exists("syntax_on") == 1 then
+		vim.cmd("syntax reset")
+	end
+	vim.o.termguicolors = true
 end
 
-local function expand_highlight_command_with(cmd, pattern, value)
-    if value == nil then
-        return
-    end
-    table.insert(cmd, string.format('%s=%s', pattern, value))
+local function to_hl(spec)
+	if not spec then
+		return {}
+	end
+	local hl = {}
+	if spec.fg then
+		hl.fg = spec.fg
+	end
+	if spec.bg then
+		hl.bg = spec.bg
+	end
+	if spec.sp then
+		hl.sp = spec.sp
+	end
+
+	local em = spec.em
+	if em == nil or em == "none" then
+		return hl
+	end
+
+	if em:find("bold") then
+		hl.bold = true
+	end
+	if em:find("italic") then
+		hl.italic = true
+	end
+	if em:find("underline") then
+		hl.underline = true
+	end
+	if em:find("strikethrough") then
+		hl.strikethrough = true
+	end
+	if em:find("undercurl") then
+		hl.undercurl = true
+	end
+	return hl
 end
 
 function M.setup(config)
-    reset()
+	reset()
 
-    config = config or {}
-    local cc = config.colors or {}
+	config = config or {}
+	local cc = config.colors or {}
 
-    local colors = {
-        smoky_black = cc.smoky_black or '#0C0C0C',
-        eerie_black = cc.eerie_black or '#1A1A1A',
-        raisin_black = cc.raisin_black or '#262626',
+	local colors = {
+		smoky_black = cc.smoky_black or "#0C0C0C",
+		eerie_black = cc.eerie_black or "#1A1A1A",
+		raisin_black = cc.raisin_black or "#262626",
 
-        dark_charcoal = cc.dark_charcoal or '#303030',
-        gray = cc.gray or '#7E7E7E',
-        quick_silver = cc.quick_silver or '#A5A5A5',
-        chinese_silver = cc.chinese_silver or '#CCCCCC',
-        anti_flash_white = cc.anti_flash_white or '#F2F2F2',
-        white = cc.white or '#FFFFFF',
-    }
+		dark_charcoal = cc.dark_charcoal or "#303030",
+		gray = cc.gray or "#7E7E7E",
+		quick_silver = cc.quick_silver or "#A5A5A5",
+		chinese_silver = cc.chinese_silver or "#CCCCCC",
+		anti_flash_white = cc.anti_flash_white or "#F2F2F2",
+		white = cc.white or "#FFFFFF",
+	}
 
-    local palette = {
-        selection = {
-            foreground = colors.smoky_black,
-            background = colors.anti_flash_white,
-        },
-        background = {
-            medium = colors.smoky_black,
-            brighter = colors.eerie_black,
-            brightest = colors.raisin_black,
-        },
-        foreground = {
-            darkest = colors.dark_charcoal,
-            darker = colors.gray,
-            dimmed = colors.quick_silver,
-            medium = colors.chinese_silver,
-            brighter = colors.anti_flash_white,
-            brightest = colors.white,
-        },
-    }
+	local palette = {
+		selection = {
+			foreground = colors.smoky_black,
+			background = colors.anti_flash_white,
+		},
+		background = {
+			medium = colors.smoky_black,
+			brighter = colors.eerie_black,
+			brightest = colors.raisin_black,
+		},
+		foreground = {
+			darkest = colors.dark_charcoal,
+			darker = colors.gray,
+			dimmed = colors.quick_silver,
+			medium = colors.chinese_silver,
+			brighter = colors.anti_flash_white,
+			brightest = colors.white,
+		},
+	}
 
-    local groups = {
-        normal = { fg = palette.foreground.medium, bg = palette.background.medium },
-        selection = { fg = palette.selection.foreground, bg = palette.selection.background },
+	local groups = {
+		normal = { fg = palette.foreground.medium, bg = palette.background.medium },
+		selection = { fg = palette.selection.foreground, bg = palette.selection.background },
 
-        darkest = { fg = palette.foreground.darkest },
-        darker = { fg = palette.foreground.darker },
-        darker_italic = { fg = palette.foreground.darker, em = 'italic' },
-        dimmed = { fg = palette.foreground.dimmed },
-        dimmed_italic = { fg = palette.foreground.dimmed, em = 'italic' },
-        medium = { fg = palette.foreground.medium },
-        medium_italic = { fg = palette.foreground.medium, em = 'italic' },
-        medium_underline = { fg = palette.foreground.medium, em = 'underline' },
-        brighter = { fg = palette.foreground.brighter },
-        brighter_italic = { fg = palette.foreground.brighter, em = 'italic' },
-        brightest = { fg = palette.foreground.brightest, em = 'none' },
-        brightest_bold = { fg = palette.foreground.brightest, em = 'bold' },
-    }
-    local higlight_groups = {
-        Normal = groups.normal,
+		darkest = { fg = palette.foreground.darkest },
+		darker = { fg = palette.foreground.darker },
+		darker_italic = { fg = palette.foreground.darker, em = "italic" },
+		dimmed = { fg = palette.foreground.dimmed },
+		dimmed_italic = { fg = palette.foreground.dimmed, em = "italic" },
+		medium = { fg = palette.foreground.medium },
+		medium_italic = { fg = palette.foreground.medium, em = "italic" },
+		medium_underline = { fg = palette.foreground.medium, em = "underline" },
+		brighter = { fg = palette.foreground.brighter },
+		brighter_italic = { fg = palette.foreground.brighter, em = "italic" },
+		brightest = { fg = palette.foreground.brightest, em = "none" },
+		brightest_bold = { fg = palette.foreground.brightest, em = "bold" },
+	}
 
-        Bold = { em = 'bold' },
-        Italic = { em = 'italic' },
-        Underlined = { em = 'underline' },
+	local matchparen = { fg = palette.foreground.medium, bg = palette.background.brightest }
 
-        Visual = groups.selection,
+	local highlight_groups = {
+		["Normal"] = groups.normal,
+		["Bold"] = { em = "bold" },
+		["Italic"] = { em = "italic" },
+		["Underlined"] = { em = "underline" },
 
-        Directory = groups.brighter,
+		["Visual"] = groups.selection,
 
-        IncSearch = groups.selection,
-        Search = groups.selection,
-        Substitute = groups.selection,
+		["Directory"] = groups.brighter,
 
-        MatchParen = groups.selection,
+		["IncSearch"] = groups.selection,
+		["Search"] = groups.selection,
+		["Substitute"] = groups.selection,
 
-        ModeMsg = groups.brighter,
-        MoreMsg = groups.brighter,
-        NonText = { fg = palette.foreground.darkest },
+		["MatchParen"] = matchparen, -- << lowered contrast
 
-        LineNr = groups.dimmed,
-        LineNrAbove = groups.darkest,
-        LineNrBelow = groups.darkest,
-        CursorLineNr = groups.darker,
+		["ModeMsg"] = groups.brighter,
+		["MoreMsg"] = groups.brighter,
+		["NonText"] = { fg = palette.foreground.darkest },
 
-        StatusLine = { fg = palette.foreground.medium, bg = palette.background.brighter },
-        StatusLineNC = { fg = palette.foreground.darkest, bg = palette.background.brighter },
-        WinSeparator = { fg = palette.foreground.darkest, bg = palette.background.medium },
-        SignColumn = groups.normal,
-        Colorcolumn = { bg = palette.background.brighter },
+		["LineNr"] = groups.dimmed,
+		["LineNrAbove"] = groups.darkest,
+		["LineNrBelow"] = groups.darkest,
+		["CursorLineNr"] = groups.darker,
 
-        TabLineFill = { bg = palette.background.brightest },
-        TabLine = { fg = palette.foreground.dimmed, bg = palette.background.brightest },
-        TabLineSel = { fg = palette.foreground.medium },
+		["StatusLine"] = { fg = palette.foreground.medium, bg = palette.background.brighter },
+		["StatusLineNC"] = { fg = palette.foreground.darkest, bg = palette.background.brighter },
+		["WinSeparator"] = { fg = palette.foreground.darkest, bg = palette.background.medium },
+		["SignColumn"] = groups.normal,
+		["ColorColumn"] = { bg = palette.background.brighter }, -- fixed casing
 
-        Pmenu = { fg = palette.foreground.medium, bg = palette.background.brightest },
-        PmenuSel = groups.selection,
-        PmenuSbar = { bg = palette.background.brightest },
+		["TabLineFill"] = { bg = palette.background.brightest },
+		["TabLine"] = { fg = palette.foreground.dimmed, bg = palette.background.brightest },
+		["TabLineSel"] = { fg = palette.foreground.medium },
 
-        Conceal = groups.darker,
-        Title = groups.brighter,
-        Question = groups.brighter,
-        SpecialKey = groups.darkest,
-        WildMenu = { fg = palette.foreground.medium, bg = palette.background.brightest },
-        Folded = { fg = palette.foreground.darker, bg = palette.background.brighter },
-        FoldColumn = { fg = palette.foreground.darkest, bg = palette.background.medium },
+		["Pmenu"] = { fg = palette.foreground.medium, bg = palette.background.brightest },
+		["PmenuSel"] = groups.selection,
+		["PmenuSbar"] = { bg = palette.background.brightest },
 
-        -- Spelling
-        SpellBad = { em = 'underline' },
-        SpellLocal = { em = 'underline' },
-        SpellCap = { em = 'underline' },
-        SpellRare = { em = 'underline' },
+		["Conceal"] = groups.darker,
+		["Title"] = groups.brighter,
+		["Question"] = groups.brighter,
+		["SpecialKey"] = groups.darkest,
+		["WildMenu"] = { fg = palette.foreground.medium, bg = palette.background.brightest },
+		["Folded"] = { fg = palette.foreground.darker, bg = palette.background.brighter },
+		["FoldColumn"] = { fg = palette.foreground.darkest, bg = palette.background.medium },
 
-        -- Syntax
-        Boolean = groups.brighter,
-        Character = groups.brighter,
-        Comment = groups.darker,
-        Conditional = groups.dimmed,
-        Constant = groups.brighter,
-        Define = groups.darker,
-        Delimiter = groups.darker,
-        Float = groups.brighter,
-        Function = groups.brighter,
-        Identifier = groups.medium,
-        Include = groups.darker,
-        Keyword = groups.dimmed_italic,
-        Label = groups.darker,
-        Number = groups.brighter,
-        Operator = groups.darker,
-        PreProc = groups.darker,
-        Repeat = groups.darker,
-        Special = groups.brighter,
-        SpecialChar = groups.brighter,
-        Statement = groups.medium,
-        StorageClass = groups.brighter,
-        String = groups.brighter_italic,
-        Structure = groups.brighter,
-        Tag = groups.medium,
-        Todo = groups.brightest,
-        Type = groups.brighter,
-        Typedef = groups.brighter,
+		["SpellBad"] = { em = "underline" },
+		["SpellLocal"] = { em = "underline" },
+		["SpellCap"] = { em = "underline" },
+		["SpellRare"] = { em = "underline" },
 
-        -- TreeSitter
-        TSAnnotation = groups.darker_italic,
-        TSAttribute = groups.darker,
-        TSBoolean = groups.brighter,
-        TSCharacter = groups.brighter,
-        TSCharacterSpecial = groups.brighter,
-        TSComment = groups.darker_italic,
-        TSConditional = groups.medium,
-        TSConstant = groups.brighter,
-        TSConstBuiltin = groups.brighter,
-        TSConstMacro = groups.darker,
-        TSConstructor = groups.brighter,
-        TSDebug = groups.medium,
-        TSDefine = groups.medium,
-        TSError = groups.brightest,
-        TSException = groups.brightest,
-        TSField = groups.medium,
-        TSFloat = groups.brighter,
-        TSFunction = groups.brighter,
-        TSFuncBuiltin = groups.brighter,
-        TSFuncMacro = groups.brighter,
-        TSInclude = groups.darker,
-        TSKeyword = groups.dimmed_italic,
-        TSKeywordFunction = groups.dimmed_italic,
-        TSKeywordOperator = groups.dimmed_italic,
-        TSKeywordReturn = groups.dimmed_italic,
-        TSLabel = groups.darker,
-        TSMethod = groups.brighter,
-        TSNamespace = groups.darker,
-        TSNone = groups.darkest,
-        TSNumber = groups.brighter,
-        TSOperator = groups.darker,
-        TSParameter = groups.medium,
-        TSParameterReference = groups.darker,
-        TSPreProc = groups.darker,
-        TSProperty = groups.medium,
-        TSPunctDelimiter = groups.darker,
-        TSPunctBracket = groups.darker,
-        TSPunctSpecial = groups.darker,
-        TSRepeat = groups.darker,
-        TSStorageClass = groups.medium,
-        TSString = groups.brighter_italic,
-        TSStringRegex = groups.brighter,
-        TSStringEscape = groups.medium,
-        TSStringSpecial = groups.medium,
-        TSSymbol = groups.brighter,
-        TSTag = groups.medium,
-        TSTagAttribute = groups.darker,
-        TSTagDelimiter = groups.darker,
-        TSText = groups.medium,
-        TSStrong = { em = 'bold' },
-        TSEmphasis = { em = 'italic' },
-        TSUnderline = { em = 'underline' },
-        TSStrike = { em = 'strikethrough' },
-        TSTitle = groups.brighter,
-        TSLiteral = groups.brighter,
-        TSURI = groups.medium_underline,
-        TSMath = groups.darker,
-        TSTextReference = groups.darker,
-        TSEnvironment = groups.darker,
-        TSEnvironmentName = groups.darker,
-        TSNote = groups.medium,
-        TSWarning = groups.brighter,
-        TSDanger = groups.brightest,
-        TSTodo = groups.brightest,
-        TSType = groups.medium,
-        TSTypeBuiltin = groups.medium,
-        TSTypeQualifier = groups.darker_italic,
-        TSTypeDefinition = groups.medium,
-        TSVariable = groups.medium,
-        TSVariableBuiltin = groups.medium,
+		["Boolean"] = groups.brighter,
+		["Character"] = groups.brighter,
+		["Comment"] = groups.darker,
+		["Conditional"] = groups.dimmed,
+		["Constant"] = groups.brighter,
+		["Define"] = groups.darker,
+		["Delimiter"] = groups.darker,
+		["Float"] = groups.brighter,
+		["Function"] = groups.brighter,
+		["Identifier"] = groups.medium,
+		["Include"] = groups.darker,
+		["Keyword"] = groups.dimmed_italic,
+		["Label"] = groups.darker,
+		["Number"] = groups.brighter,
+		["Operator"] = groups.darker,
+		["PreProc"] = groups.darker,
+		["Repeat"] = groups.darker,
+		["Special"] = groups.brighter,
+		["SpecialChar"] = groups.brighter,
+		["Statement"] = groups.medium,
+		["StorageClass"] = groups.brighter,
+		["String"] = groups.brighter_italic,
+		["Structure"] = groups.brighter,
+		["Tag"] = groups.medium,
+		["Todo"] = groups.brightest,
+		["Type"] = groups.brighter,
+		["Typedef"] = groups.brighter,
 
-        -- Diff
-        DiffAdd = { bg = palette.background.brighter },
-        DiffAdded = { fg = palette.foreground.medium },
+		["@annotation"] = groups.darker_italic,
+		["@attribute"] = groups.darker,
+		["@boolean"] = groups.brighter,
+		["@character"] = groups.brighter,
+		["@character.special"] = groups.brighter,
+		["@comment"] = groups.darker_italic,
+		["@conditional"] = groups.medium,
+		["@constant"] = groups.brighter,
+		["@constant.builtin"] = groups.brighter,
+		["@constant.macro"] = groups.darker,
+		["@constructor"] = groups.brighter,
+		["@debug"] = groups.medium,
+		["@error"] = groups.brightest,
+		["@exception"] = groups.brightest,
+		["@field"] = groups.medium,
+		["@float"] = groups.brighter,
+		["@function"] = groups.brighter,
+		["@function.builtin"] = groups.brighter,
+		["@function.macro"] = groups.brighter,
+		["@include"] = groups.darker,
+		["@keyword"] = groups.dimmed_italic,
+		["@keyword.function"] = groups.dimmed_italic,
+		["@keyword.operator"] = groups.dimmed_italic,
+		["@keyword.return"] = groups.dimmed_italic,
+		["@label"] = groups.darker,
+		["@method"] = groups.brighter,
+		["@namespace"] = groups.darker,
+		["@none"] = groups.darkest,
+		["@number"] = groups.brighter,
+		["@operator"] = groups.darker,
+		["@parameter"] = groups.medium,
+		["@parameter.reference"] = groups.darker,
+		["@preproc"] = groups.darker,
+		["@property"] = groups.medium,
+		["@punctuation.delimiter"] = groups.darker,
+		["@punctuation.bracket"] = groups.darker,
+		["@punctuation.special"] = groups.darker,
+		["@repeat"] = groups.darker,
+		["@storageclass"] = groups.medium,
+		["@string"] = groups.brighter_italic,
+		["@string.regex"] = groups.brighter,
+		["@string.escape"] = groups.medium,
+		["@string.special"] = groups.medium,
+		["@symbol"] = groups.brighter,
+		["@tag"] = groups.medium,
+		["@tag.attribute"] = groups.darker,
+		["@tag.delimiter"] = groups.darker,
+		["@text"] = groups.medium,
+		["@text.strong"] = { em = "bold" },
+		["@text.emphasis"] = { em = "italic" },
+		["@text.underline"] = { em = "underline" },
+		["@text.strike"] = { em = "strikethrough" },
+		["@text.title"] = groups.brighter,
+		["@text.literal"] = groups.brighter,
+		["@text.uri"] = groups.medium_underline,
+		["@text.math"] = groups.darker,
+		["@text.reference"] = groups.darker,
+		["@environment"] = groups.darker,
+		["@environment.name"] = groups.darker,
+		["@note"] = groups.medium,
+		["@warning"] = groups.brighter,
+		["@danger"] = groups.brightest,
+		["@todo"] = groups.brightest,
+		["@type"] = groups.medium,
+		["@type.builtin"] = groups.medium,
+		["@type.qualifier"] = groups.darker_italic,
+		["@type.definition"] = groups.medium,
+		["@variable"] = groups.medium,
+		["@variable.builtin"] = groups.medium,
 
-        DiffDelete = { fg = palette.background.brighter, bg = palette.background.brighter },
-        DiffRemoved = { fg = palette.foreground.darker },
+		["DiffAdd"] = { bg = palette.background.brighter },
+		["DiffAdded"] = { fg = palette.foreground.medium },
+		["DiffDelete"] = { fg = palette.background.brighter, bg = palette.background.brighter },
+		["DiffRemoved"] = { fg = palette.foreground.darker },
+		["DiffChange"] = { bg = palette.background.brighter },
+		["DiffText"] = { fg = palette.background.medium, bg = palette.foreground.darker, em = "bold" },
+		["DiffLine"] = { fg = palette.foreground.darker },
 
-        DiffChange = { bg = palette.background.brighter },
-        DiffText = { fg = palette.background.medium, bg = palette.foreground.darker, em = 'bold' },
+		["TelescopeSelectionCaret"] = { bg = palette.background.brightest },
+		["TelescopeSelection"] = { bg = palette.background.brightest },
+		["TelescopeMatching"] = groups.brightest_bold,
 
-        DiffLine = { fg = palette.foreground.darker },
+		["netrwMarkFile"] = { bg = palette.background.brightest },
+	}
 
-        -- Telescope
-        TelescopeSelectionCaret = { bg = palette.background.brightest },
-        TelescopeSelection = { bg = palette.background.brightest },
-        TelescopeMatching = groups.brightest_bold,
-
-        -- Netrw
-        netrwMarkFile = { bg = palette.background.brightest },
-    }
-
-    for group, set in pairs(higlight_groups) do
-        local cmd = { 'hi', group }
-
-        expand_highlight_command_with(cmd, 'guifg', set.fg)
-        expand_highlight_command_with(cmd, 'guibg', set.bg)
-        expand_highlight_command_with(cmd, 'gui', set.em or 'none')
-        expand_highlight_command_with(cmd, 'sp', set.sp)
-
-        vim.cmd(table.concat(cmd, ' '))
-    end
+	for group, spec in pairs(highlight_groups) do
+		vim.api.nvim_set_hl(0, group, to_hl(spec))
+	end
 end
 
 return M
